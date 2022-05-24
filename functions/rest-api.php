b81db6e91f22ded23450d423a4f0051f1992a1bd
Martin Stepanek <martin@stepanek.sk>
2022-05-24 11:27:27 +0200

    fix: include integration methods files

--- functions/rest-api.php
+++ functions/rest-api.php
@@ -2,6 +2,8 @@
 
 require_once get_template_directory() . '/lib/rest-api/get-blog-posts.php';
 require_once get_template_directory() . '/lib/rest-api/get-language-codes.php';
+require_once get_template_directory() . '/lib/rest-api/integration-methods-variables.php';
 require_once get_template_directory() . '/lib/rest-api/menus.php';
 require_once get_template_directory() . '/lib/rest-api/metaboxes.php';
+require_once get_template_directory() . '/lib/rest-api/rest-api-get-integration-methods.php';
 require_once get_template_directory() . '/lib/rest-api/wpml-rest-api.php';
