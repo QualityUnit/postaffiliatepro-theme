99c303b88925327d1a32f8cd7c1a1917f7e53d71
Martin Stepanek <martin@stepanek.sk>
2022-07-11 09:32:01 +0200

    feat: implement urlslab plugin as related resources source

--- templates/content-single.php
+++ templates/content-single.php
@@ -140,14 +140,13 @@
 					<?php endif; ?>
 					<?php wp_reset_postdata(); ?>
 				</div>
-				<?php if ( ICL_LANGUAGE_CODE === 'en' ) { ?>
-					<div class="Post__content__resources Post__m__negative">
-						<div class="Post__sidebar__title h4"><?php _e( 'Related Resources', 'ms' ); ?></div>
-						<div class="SimilarSources">
-							<?php echo do_shortcode( '[similarsources]' ); ?>
-						</div>
+
+				<div class="Post__content__resources Post__m__negative">
+					<div class="Post__sidebar__title h4"><?php _e( 'Related Resources', 'ms' ); ?></div>
+					<div class="SimilarSources">
+						<?php echo do_shortcode( '[urlslab-related-resources]' ); ?>
 					</div>
-				<?php } ?>
+				</div>
 			</div>
 		</div>
 	</div>
