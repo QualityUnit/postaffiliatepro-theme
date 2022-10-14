489ab57a6bf67bb5969f05d3aea0ecd7c75d4651
Jozef Remeň <jozef.remen@gmail.com>
2022-10-14 09:06:41 +0200

    fix: affiliate program directory archive

--- templates/content-archive-ms_directory.php
+++ templates/content-archive-ms_directory.php
@@ -1,85 +1,63 @@
-<?php // @codingStandardsIgnoreLine
-
-set_custom_source( 'dropdown', 'js' );
-
-?>
-
+<?php // @codingStandardsIgnoreLine ?>
 <div id="archive" class="Archive" itemscope itemtype="https://schema.org/DefinedTermSet">
-	<?php $categories = get_categories( array( 'taxonomy' => 'ms_directory_categories' ) ); ?>
-	<div class="wrapper Archive__header Archive__header--directory">
+	<div class="wrapper Archive__header Archive__header--glossary">
 		<?php if ( is_tax( 'ms_directory_categories' ) ) { ?>
-			<h1 class="Archive__header__title--directory" itemprop="name"><?php single_cat_title(); ?></h1>
-			<div class="Archive__header__subtitle--directory"><p itemprop="description"><?php the_archive_description(); ?></p></div>
+			<h1 class="Archive__header__title" itemprop="name"><?php single_cat_title(); ?></h1>
+			<div class="Archive__header__subtitle"><p itemprop="description"><?php the_archive_description(); ?></p></div>
 		<?php } else { ?>
-		<h1 class="Archive__header__title--directory" itemprop="name"><?php _e( 'Affiliate Program', 'ms' ); ?> <span class=" highlight highlight-splash2"><?php _e( 'Directory', 'ms' ); ?></span></h1>
-		<p class="Archive__header__subtitle--directory" itemprop="description"><?php _e( 'A directory of companies and affiliate programs', 'ms' ); ?></p>
+		<h1 class="Archive__header__title" itemprop="name"><?php _e( 'Affiliate Program Directory', 'ms' ); ?></h1>
+		<p class="Archive__header__subtitle" itemprop="description"><?php _e( "Looking for an affiliate program to join? That's why we made a dedicated page where you can find information about joinable affiliate programs. Take a look at our directory of affiliate programs and find a suitable program for you. Click any of the links below to find specific information and contacts.", 'ms' ); ?></p>
 			<?php } ?>
-		<a class="Archive__header__button Button Button--full">
-			<?php _e( 'I want to be in list', 'ms' ); ?>
-		</a>
-		<div class="Archive__header__filter">
-			<div class="searchField">
-				<img class="searchField__icon" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon-search_new_v2.svg" alt="<?php _e( 'Search', 'ms' ); ?>" />
-				<input type="search" class="search" placeholder="<?php _e( 'Search affiliate program', 'ms' ); ?>" maxlength="20">
-			</div>
-			<div class="Archive__header__dropdown">
-				<button class="dropdown">
-					<span><?php _e( 'Category', 'ms' ); ?></span>
-					<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon-arrow.svg" alt="<?php _e( 'dropdown arrow', 'ms' ); ?>">
-				</button>
-				<div class="Archive__header__dropdown--menu">
-					<ul>
-						<?php foreach ( $categories as $category ) { ?>
-							<li><a href="#<?= esc_attr( $category->slug ); ?>" title="<?= esc_attr( $category->name ); ?>"><?= esc_html( $category->slug ); ?></a></li>
-						<?php } ?>
-					</ul>
-				</div>
+	</div>
+
+	<div class="Box Box--gray Archive__filter">
+		<div class="wrapper">
+			<div class="Archive__filter__item">
+				<ul>
+					<?php $categories = get_categories( array( 'taxonomy' => 'ms_directory_categories' ) ); ?>
+					<?php foreach ( $categories as $category ) { ?>
+						<li><a href="#<?= esc_attr( $category->slug ); ?>" title="<?= esc_attr( $category->name ); ?>"><?= esc_html( $category->name ); ?></a></li>
+					<?php } ?>
+				</ul>
 			</div>
 		</div>
 	</div>
-	<div class="wrapper Archive__directory">
-		<ul class="Archive__directory__container">
-			<?php
-			$query_glossary_posts = new WP_Query(
-				array(
-					'ms_directory_categories' => 'u',
-					// @codingStandardsIgnoreLine
-					'posts_per_page' => 500,
-					'orderby'                 => 'title',
-					'order'                   => 'ASC',
-				)
-			);
-			while ( $query_glossary_posts->have_posts() ) :
-				$query_glossary_posts->the_post();
-				?>
-				<?php
-				if ( get_post_meta( get_the_ID(), 'mb_directory_mb_directory_company-name', true ) ) {
-					$post_title = get_post_meta( get_the_ID(), 'mb_directory_mb_directory_company-name', true );
-				} else {
-					$post_title = get_the_title();
-				}
-				$post_description = get_post_meta( get_the_ID(), 'company_description', true );
-				?>
-				<li class="Archive__directory__container__item" itemscope itemtype="https://schema.org/DefinedTerm">
-					<img class="Archive__directory__container__item--screenshot domain-placeholder" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/domain_placeholder.svg" alt="<?php _e( 'domain screenshot', 'ms' ); ?>">
-					<h5 class="Archive__directory__container__item--title" itemprop="name"><?php echo esc_html( $post_title ); ?></h5>
-					<span class="Archive__directory__container__item--description"><?php echo esc_html( $post_description ); ?></span>
-				</li>
-			<?php endwhile; ?>
-			<?php wp_reset_postdata(); ?>
-		</ul>
-		<div class="Archive__directory__sidebar">
-			<div class="Archive__directory__sidebar__container">
-				<h3 class="Archive__directory__sidebar__container--title"><?php _e( 'Would you like to be ', 'ms' ); ?><span class="highlight highlight-splash"><?php _e( 'included?', 'ms' ); ?></span></h3>
-				<p class="Archive__directory__sidebar__container--text"><?php _e( 'Contact us and we’ll add your company and affiliate program to our comprehensive directory.', 'ms' ); ?></p>
-				<button class="Button Button--full"><?php _e( 'Join to the list', 'ms' ); ?></button>
-			</div>
-			<div class="Archive__directory__sidebar__categories">
-				<h5><?php _e( 'Affiliate program categories', 'ms' ); ?></h5>
-				<?php foreach ( $categories as $category ) { ?>
-					<span><?= esc_html( $category->name ); ?></span>
+
+	<div class="wrapper Archive__container Archive__container--glossary">
+		<div class="Archive__container__content list">
+			<?php $categories = get_categories( array( 'taxonomy' => 'ms_directory_categories' ) ); ?>
+			<?php foreach ( $categories as $category ) { ?>
+				<div id="<?= esc_attr( $category->slug ); ?>" class="Archive__container__content__item">
+					<h2 class="Archive__container__content__item__title"><?= esc_html( $category->name ); ?></h2>
+					<div class="Archive__container__content__item__content">
+						<ul>
+							<?php
+							$query_glossary_posts = new WP_Query(
+								array(
+									'ms_directory_categories' => $category->slug,
+									// @codingStandardsIgnoreLine
+									'posts_per_page' => 500,
+									'orderby'        => 'title',
+									'order'          => 'ASC',
+								)
+							);
+							while ( $query_glossary_posts->have_posts() ) :
+								$query_glossary_posts->the_post();
+								?>
+								<?php
+								if ( get_post_meta( get_the_ID(), 'mb_directory_mb_directory_company-name', true ) ) {
+									$post_title = get_post_meta( get_the_ID(), 'mb_directory_mb_directory_company-name', true );
+								} else {
+									$post_title = get_the_title();
+								}
+								?>
+								<li itemscope itemtype="https://schema.org/DefinedTerm"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( $post_title ); ?>" itemprop="url"><span itemprop="name"><?php echo esc_html( $post_title ); ?></span></a></li>
+							<?php endwhile; ?>
+							<?php wp_reset_postdata(); ?>
+						</ul>
+					</div>
+				</div>
 			<?php } ?>
-			</div>
 		</div>
 	</div>
 </div>
