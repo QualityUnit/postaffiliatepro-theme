1330b4e1a9d816e332dd1e9b1137663dcdffcaf2
Jozef Remeň <jozef.remen@gmail.com>
2022-10-26 09:54:58 +0200

    fix: removing lazyload from signup critical scripts, fixing loading of scripts by sidebar

--- lib/shortcodes/login.php
+++ lib/shortcodes/login.php
@@ -35,7 +35,6 @@ function ms_login() {
 
 	<?php // @codingStandardsIgnoreStart ?>
 	<script defer data-src="https://www.google.com/recaptcha/api.js?render=6LddyswZAAAAAJrOnNWj_jKRHEs_O_I312KKoMDJ"></script>
-	<script defer data-src="<?= esc_url( get_template_directory_uri() ) . '/assets/scripts/static/jquery.alphanum.js' ?>"></script>
 	<?php if ( ICL_LANGUAGE_CODE === 'en' ) { ?>
 		<script defer data-src="<?= esc_url( get_template_directory_uri() ) . '/assets/scripts/static/login_en.js' ?>"></script>
 	<?php } elseif ( ICL_LANGUAGE_CODE === 'de' ) { ?>
@@ -64,6 +63,7 @@ function ms_login() {
 	<?php // @codingStandardsIgnoreEnd ?>
 
 	<?php
+	wp_enqueue_script( 'jqueryalphanum', get_template_directory_uri() . '/assets/scripts/static/jquery.alphanum.js', array( 'jquery' ), THEME_VERSION, true );
 	set_custom_source( 'components/Signup' );
 	return ob_get_clean();
 }
--- lib/shortcodes/signup-form.php
+++ lib/shortcodes/signup-form.php
@@ -71,8 +71,6 @@ function ms_signup_form() {
 	<?php // @codingStandardsIgnoreStart ?>
 
 	<script defer data-src="https://www.google.com/recaptcha/api.js?render=6LddyswZAAAAAJrOnNWj_jKRHEs_O_I312KKoMDJ"></script>
-	<script defer data-src="<?= esc_url( get_template_directory_uri() ) . '/assets/scripts/static/jquery.cookie.js' ?>"></script>
-	<script defer data-src="<?= esc_url( get_template_directory_uri() ) . '/assets/scripts/static/jquery.alphanum.js' ?>"></script>
 	<script defer data-src="<?= esc_url( get_template_directory_uri() ) . '/assets/scripts/static/source.js' ?>"></script>
 	<?php if ( ICL_LANGUAGE_CODE === 'en' ) { ?>
 		<script defer data-src="<?= esc_url( get_template_directory_uri() ) . '/assets/scripts/static/crm_en.js' ?>"></script>
@@ -102,6 +100,8 @@ function ms_signup_form() {
 	<?php // @codingStandardsIgnoreEnd ?>
 
 	<?php
+	wp_enqueue_script( 'jquerycookie', get_template_directory_uri() . '/assets/scripts/static/jquery.cookie.js', array( 'jquery' ), THEME_VERSION, true );
+	wp_enqueue_script( 'jqueryalphanum', get_template_directory_uri() . '/assets/scripts/static/jquery.alphanum.js', array( 'jquery' ), THEME_VERSION, true );
 	set_custom_source( 'components/Signup' );
 	return ob_get_clean();
 }
