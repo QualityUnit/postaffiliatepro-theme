e4edf20ea882999b0d7ce884a3adbc359dad960f
Jozef Remeň <jozef.remen@gmail.com>
2023-01-30 11:10:20 +0100

    fix: phpcbf

--- functions/elementor-content-filters.php
+++ functions/elementor-content-filters.php
@@ -11,10 +11,10 @@ function add_awards_to_header( $content ) {
 	libxml_use_internal_errors( true );
 	$dom->loadHTML( mb_convert_encoding( $content, 'HTML-ENTITIES', 'UTF-8' ) );
 	libxml_clear_errors();
-	$xpath      = new DOMXPath( $dom );
+	$xpath       = new DOMXPath( $dom );
 	$hero_header = $xpath->query( ".//*[contains(@class, 'heroHeadline--bg')]" );
-  $awards = $dom->createDocumentFragment();
-  $awards->appendXML('<div class="AwardsHeroHeader"><img class="AwardsHeroHeader__text" src="' . get_template_directory_uri(  ) . '/assets/images/heroHeadline_award_badges_text.svg" alt="Awards" />[awards_small posts=3]</div>');
+	$awards      = $dom->createDocumentFragment();
+	$awards->appendXML( '<div class="AwardsHeroHeader"><img class="AwardsHeroHeader__text" src="' . get_template_directory_uri() . '/assets/images/heroHeadline_award_badges_text.svg" alt="Awards" />[awards_small posts=3]</div>' );
 
 	// @codingStandardsIgnoreStart
 	foreach ( $hero_header as $header ) {
