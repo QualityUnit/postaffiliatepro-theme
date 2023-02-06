94b75db653d064f7091657adcd515905eb6f01d7
Jozef Remeň <jozef.remen@gmail.com>
2023-02-06 12:38:24 +0100

    feat: directory affiliate notice

--- templates/content-single-ms_directory.php
+++ templates/content-single-ms_directory.php
@@ -170,6 +170,11 @@ if ( 'voluum' === $software_id ) {
 				</ul>
 			</div>
 
+			<div class="Post__importantNote">
+				<svg class="icon icon-info-solid"><use xlink:href="<?= get_template_directory_uri( ); ?>/assets/images/icons.svg#info-solid"></use></svg>
+				<p>Post Affiliate Pro offers a comprehensive list of affiliate contacts. We are not connected to any of the companies listed here. Feel free to contact us if you want us to update any information.</p>
+			</div>
+
 			<h1><?php the_title(); ?></h1>
 
 			<div class="Content">
