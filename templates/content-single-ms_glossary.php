65eb29eb305e324920d39971679dcbbdc1fcf800
thujcak <thujcak@qualityunit.com>
2023-10-05 12:21:39 +0200

    feat: add compact header to glossary archive,single and change layout as on LA

--- templates/content-single-ms_glossary.php
+++ templates/content-single-ms_glossary.php
@@ -1,57 +1,34 @@
-<?php // @codingStandardsIgnoreLine ?>
-<div class="Post" itemscope itemtype="http://schema.org/TechArticle">
+<?php // @codingStandardsIgnoreLine
+$page_header_logo = array(
+	'src' => get_template_directory_uri() . '/assets/images/icon-book.svg?ver=' . THEME_VERSION,
+	'alt' => __( 'Glossary', 'ms' ),
+);
+if ( has_post_thumbnail() ) {
+	$page_header_logo['src'] = get_the_post_thumbnail_url( $post, 'logo_thumbnail' );
+}
+$page_header_args = array(
+	'image' => array(
+		'src' => get_template_directory_uri() . '/assets/images/compact-header-glossary.png?ver=' . THEME_VERSION,
+		'alt' => get_the_title(),
+	),
+	'logo'  => $page_header_logo,
+	'title' => get_the_title(),
+	'text'  => do_shortcode( '[urlslab-generator id="6"]' ),
+	'toc'   => true,
+);
+?>
+<div class="Post Post--sidebar-right" itemscope itemtype="http://schema.org/TechArticle">
 	<meta itemprop="url" content="<?= esc_url( get_permalink() ); ?>">
-	<span itemprop="publisher" itemscope itemtype="http://schema.org/Organization"><meta itemprop="name" content="PostAffiliatePro"></span>
+	<span itemprop="publisher" itemscope itemtype="http://schema.org/Organization"><meta itemprop="name" content="LiveAgent"></span>
+	<?php get_template_part( 'lib/custom-blocks/compact-header', null, $page_header_args ); ?>
 
-	<div class="Post__header customer-support-glossary ">
-		<div class="wrapper__wide"></div>
-	</div>
-
-	<div class="wrapper__wide Post__container">
+	<div class="wrapper Post__container">
 		<div class="Post__sidebar">
-			<?php if ( boolval( get_post_meta( get_the_ID(), 'mb_glossary_mb_glossary_related-articles', true ) ) ) { ?>
-				<div class="Post__sidebar__related">
-					<h4 class="Post__sidebar__title"><?php _e( 'Related Articles', 'ms' ); ?></h4>
-					<?= wp_kses_post( get_post_meta( get_the_ID(), 'mb_glossary_mb_glossary_related-articles', true ) ) ?>
-				</div>
-			<?php } ?>
-
-			<?php if ( sidebar_toc() !== false ) { ?>
-				<div class="SidebarTOC-wrapper">
-					<div class="SidebarTOC Post__SidebarTOC">
-						<strong class="SidebarTOC__title"><?php _e( 'Contents', 'ms' ); ?></strong>
-						<div class="SidebarTOC__slider slider splide">
-							<div class="splide__track">
-								<ul class="SidebarTOC__content splide__list">
-									<?= wp_kses_post( sidebar_toc() ); ?>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			<?php } ?>
+			<div class="Signup__sidebar-wrapper">
+				<?= do_shortcode( '[signup-sidebar js-sticky="true"]' ); ?>
+			</div>
 		</div>
-
-		<div class="Signup__sidebar-wrapper">
-			<?= do_shortcode( '[signup-sidebar]' ); ?>
-		</div>
-
 		<div class="Post__content">
-			<div class="Post__logo">
-				<?php if ( has_post_thumbnail() ) { ?>
-					<?php the_post_thumbnail( 'logo_thumbnail' ); ?>
-				<?php } else { ?>
-					<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon-book.svg" alt="<?php _e( 'Glossary', 'ms' ); ?>">
-				<?php } ?>
-			</div>
-			<div class="Post__content__breadcrumbs">
-				<ul>
-					<li><a href="<?php _e( '/affiliate-marketing-glossary/', 'ms' ); ?>"><?php _e( 'Affiliate Marketing Glossary', 'ms' ); ?></a></li>
-					<li><?php the_title(); ?></li>
-				</ul>
-			</div>
-
-			<h1 itemprop="name"><?php the_title(); ?></h1>
 
 			<div class="Content" itemprop="articleBody">
 				<?php the_content(); ?>
@@ -59,12 +36,43 @@
 				<?php echo do_shortcode( '[urlslab-faq]' ); ?>
 
 				<div class="Post__buttons">
-					<a href="<?php _e( '/affiliate-marketing-glossary/', 'ms' ); ?>" class="Button Button--outline Button--back" onclick="_paq.push(['trackEvent', 'Activity', 'Glossary', 'Back to Glossary'])"><span></span><?php _e( 'Back to Glossary', 'ms' ); ?></span></a>
+					<a href="<?php _e( '/customer-support-glossary/', 'ms' ); ?>" class="Button Button--outline Button--back" onclick="_paq.push(['trackEvent', 'Activity', 'Glossary', 'Back to Glossary'])"><span><?php _e( 'Back to Glossary', 'ms' ); ?></span></a>
 
 					<a href="<?php _e( '/trial/', 'ms' ); ?>" class="Button Button--full" onclick="_paq.push(['trackEvent', 'Activity', 'Glossary', 'Sign Up Trial'])">
 						<span><?php _e( 'Create account for FREE', 'ms' ); ?></span>
 					</a>
 				</div>
+
+				<?php
+				$shortcode_text = do_shortcode( '[urlslab-generator id="1"]' );
+
+				if ( ! empty( $shortcode_text ) ) {
+					?>
+
+					<div class="BlockDiscover BlockDiscover--expert">
+						<p class="BlockDiscover__title"><?php _e( 'Expert note', 'ms' ); ?></p>
+						<p class="BlockDiscover__text"><?php echo esc_html( $shortcode_text ); ?></p>
+
+						<div class="BlockDiscover__buttons">
+							<a href="<?php _e( '/trial/', 'ms' ); ?>" class="Button Button--knockout">
+								<span><?php _e( 'Try LiveAgent', 'ms' ); ?></span>
+							</a>
+							<a href="<?php _e( '/demo/', 'ms' ); ?>" class="Button Button--outline Button--outline__white">
+								<span><?php _e( 'Schedule a Demo', 'ms' ); ?></span>
+							</a>
+						</div>
+
+						<div class="BlockDiscover__agent">
+							<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/agent-saxon.png" alt="<?php _e( 'Andrej Saxon', 'ms' ); ?>" class="BlockDiscover__agent__photo">
+							<div>
+								<div class="BlockDiscover__agent__name"><?php _e( 'Andrej Saxon', 'ms' ); ?></div>
+								<div class="BlockDiscover__agent__position"><?php _e( 'Sales manager', 'ms' ); ?></div>
+							</div>
+						</div>
+					</div>
+
+					<?php set_custom_source( 'shortcodes/BlockDiscover' ); ?>
+				<?php } ?>
 
 				<div class="Post__content__resources">
 					<div class="Post__sidebar__title h4"><?php _e( 'Related Articles', 'ms' ); ?></div>
