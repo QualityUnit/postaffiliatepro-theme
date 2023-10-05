0eeae1bd6c879bd915f4f5e8f6f26b3b7fbeaab3
thujcak <thujcak@qualityunit.com>
2023-10-05 11:23:15 +0200

    feat: add compact header to features single and change layout as on LA

--- templates/content-single-ms_features.php
+++ templates/content-single-ms_features.php
@@ -2,104 +2,93 @@
 $current_lang    = apply_filters( 'wpml_current_language', null );
 $header_category = get_en_category( 'ms_features', $post->ID );
 do_action( 'wpml_switch_language', $current_lang );
+$la_pricing_url   = __( '/pricing/', 'ms' );
+$page_header_logo = array(
+	'src' => get_template_directory_uri() . '/assets/images/icon-custom-post_type.svg' . THEME_VERSION,
+	'alt' => __( 'Integration', 'ms' ),
+);
+if ( has_post_thumbnail() ) {
+	$page_header_logo['src'] = get_the_post_thumbnail_url( $post, 'logo_thumbnail' );
+}
+$page_header_image = 'features-category_' . $header_category . '.png';
+$page_header_args  = array(
+	'image' => array(
+		'src' => get_template_directory_uri() . '/assets/images/' . $page_header_image . '?ver=' . THEME_VERSION,
+		'alt' => get_the_title(),
+	),
+	'logo'  => $page_header_logo,
+	'title' => get_the_title(),
+	'text'  => do_shortcode( '[urlslab-generator id="6"]' ),
+	'toc'   => true,
+);
+$current_id        = apply_filters( 'wpml_object_id', $post->ID, 'ms_features' );
+$categories        = get_the_terms( $current_id, 'ms_features_categories' );
+$categories_url    = get_post_type_archive_link( 'ms_features' );
+if ( $categories && $categories_url ) {
+	$new_tags = array(
+		'title' => __( 'Categories', 'ms' ),
+	);
+	foreach ( $categories as $category ) {
+		$new_tags['list'][] = array(
+			'href'  => $categories_url . '#' . $category->slug,
+			'title' => $category->name,
+		);
+	}
+	if ( isset( $new_tags['list'] ) ) {
+		$page_header_args['tags'][] = $new_tags;
+	}
+}
+if ( get_post_meta( get_the_ID(), 'mb_features_mb_features_plan', true ) ) {
+	$new_tags = array(
+		'title' => __( 'Available in', 'ms' ),
+	);
+	foreach ( get_post_meta( get_the_ID(), 'mb_features_mb_features_plan', true ) as $item ) {
+		if ( 'ticket' === $item ) {
+			$new_tags['list'][] = array(
+				'href'  => $la_pricing_url,
+				'title' => __( 'Small', 'ms' ),
+			);
+		}
+		if ( 'ticket-chat' === $item ) {
+			$new_tags['list'][] = array(
+				'href'  => $la_pricing_url,
+				'title' => __( 'Medium', 'ms' ),
+			);
+		}
+		if ( 'all-inclusive' === $item ) {
+			$new_tags['list'][] = array(
+				'href'  => $la_pricing_url,
+				'title' => __( 'Large', 'ms' ),
+			);
+		}
+		if ( 'self-hosted' === $item ) {
+			$new_tags['list'][] = array(
+				'href'  => $la_pricing_url,
+				'title' => __( 'Self-Hosted', 'ms' ),
+			);
+		}
+	}
+	if ( isset( $new_tags['list'] ) ) {
+		$page_header_args['tags'][] = $new_tags;
+	}
+}
 ?>
 
-
-<div class="Post" itemscope itemtype="http://schema.org/TechArticle">
+<div class="Post Post--sidebar-right" itemscope itemtype="http://schema.org/TechArticle">
 	<meta itemprop="url" content="<?= esc_url( get_permalink() ); ?>">
-	<span itemprop="publisher" itemscope itemtype="http://schema.org/Organization"><meta itemprop="name" content="PostAffiliatePro"></span>
+	<span itemprop="publisher" itemscope itemtype="http://schema.org/Organization"><meta itemprop="name" content="LiveAgent"></span>
 
-	<div class="Post__header features <?= esc_attr( $header_category ); ?>">
-		<div class="wrapper__wide"></div>
-	</div>
+	<?php get_template_part( 'lib/custom-blocks/compact-header', null, $page_header_args ); ?>
 
-	<div class="wrapper__wide Post__container">
+	<div class="wrapper Post__container">
 		<div class="Post__sidebar">
-			<div class="Post__sidebar__categories">
-				<h4 class="Post__sidebar__title"><?php _e( 'Categories', 'ms' ); ?></h4>
-				<ul class="CategoryTags">
-					<?php
-					$current_id = apply_filters( 'wpml_object_id', $post->ID, 'ms_features' );
-					$categories = get_the_terms( $current_id, 'ms_features_categories' );
-
-					if ( $categories ) {
-						foreach ( $categories as $category ) {
-							$category_id    = $category->term_id;
-							$category_name  = $category->name;
-							$category_color = get_term_meta( $category_id, 'category_color', true );
-							?>
-							<li class="CategoryTag <?= esc_attr( $category_color ); ?>">
-								<a href="../#<?= esc_attr( $category->slug ); ?>" title="<?= esc_attr( $category_name ); ?>"><?= esc_html( $category_name ); ?></a>
-							</li>
-							<?php
-						}
-					}
-					?>
-				</ul>
+			<div class="Signup__sidebar-wrapper">
+				<?= do_shortcode( '[signup-sidebar js-sticky="true"]' ); ?>
 			</div>
-
-			<div class="Post__sidebar__available">
-				<h4 class="Post__sidebar__title"><?php _e( 'Available in', 'ms' ); ?></h4>
-				<ul>
-					<?php
-					if ( get_post_meta( get_the_ID(), 'mb_features_mb_features_plan', true ) ) {
-						foreach ( get_post_meta( get_the_ID(), 'mb_features_mb_features_plan', true ) as $item ) {
-							if ( 'pro' === $item ) {
-								echo "<li class='" . esc_attr( $item ) . "'>Post Affiliate Pro</li>";
-							}
-							if ( 'ultimate' === $item ) {
-								echo "<li class='" . esc_attr( $item ) . "'>Post Affiliate Pro Ultimate</li>";
-							}
-							if ( 'network' === $item ) {
-								echo "<li class='" . esc_attr( $item ) . "'>Post Affiliate Network</li>";
-							}
-						}
-					}
-					?>
-				</ul>
-			</div>
-
-			<?php if ( sidebar_toc() !== false ) { ?>
-				<div class="SidebarTOC-wrapper">
-					<div class="SidebarTOC Post__SidebarTOC">
-						<strong class="SidebarTOC__title"><?php _e( 'Contents', 'ms' ); ?></strong>
-						<div class="SidebarTOC__slider slider splide">
-							<div class="splide__track">
-								<ul class="SidebarTOC__content splide__list">
-									<?= wp_kses_post( sidebar_toc() ); ?>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			<?php } ?>
 		</div>
-
-		<div class="Signup__sidebar-wrapper">
-			<?= do_shortcode( '[signup-sidebar]' ); ?>
-		</div>
-
-
 		<div class="Post__content">
-			<div class="Post__logo Post__logo--features">
-				<?php if ( has_post_thumbnail() ) { ?>
-					<?php the_post_thumbnail( 'logo_thumbnail' ); ?>
-				<?php } else { ?>
-					<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon-custom-post_type.svg" alt="<?php _e( 'Integration', 'ms' ); ?>">
-				<?php } ?>
-			</div>
-			<div class="Post__content__breadcrumbs">
-				<ul>
-					<li><a href="<?php _e( '/features/', 'ms' ); ?>"><?php _e( 'Features', 'ms' ); ?></a></li>
-					<li><?php the_title(); ?></li>
-				</ul>
-			</div>
-
-			<h1 itemprop="name"><?php the_title(); ?></h1>
-
 			<div class="Content" itemprop="articleBody">
 				<?php the_content(); ?>
-				<?php echo do_shortcode( '[urlslab-faq]' ); ?>
 
 				<div class="Post__content__resources">
 					<div class="Post__sidebar__title h4"><?php _e( 'Related Articles', 'ms' ); ?></div>
