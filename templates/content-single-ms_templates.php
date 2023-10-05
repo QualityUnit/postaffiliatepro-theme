9e50a019e50f1bbda56321feee1e2620a92c7b16
thujcak <thujcak@qualityunit.com>
2023-10-05 12:16:04 +0200

    feat: add compact header to templates archive,single and change layout as on LA

--- templates/content-single-ms_templates.php
+++ templates/content-single-ms_templates.php
@@ -1,105 +1,124 @@
 <?php // @codingStandardsIgnoreLine
-set_source( 'single-post', 'common/splide', 'css' );
-set_source( 'single-post', 'splide', 'js' );
-set_source( 'single-post', 'sidebar_toc', 'js' );
-set_source( 'single-post', 'custom_lightbox', 'js' );
-$posttitle          = get_the_title();
-$posttitle_filtered = str_replace( '^', '', $posttitle );
-$current_lang       = apply_filters( 'wpml_current_language', null );
-$header_category    = get_en_category( 'ms_templates', $post->ID );
+$current_lang    = apply_filters( 'wpml_current_language', null );
+$header_category = get_en_category( 'ms_templates', $post->ID );
 do_action( 'wpml_switch_language', $current_lang );
+
+$page_header_logo = array(
+	'src' => get_template_directory_uri() . '/assets/images/icon-book.svg?ver=' . THEME_VERSION,
+	'alt' => __( 'Templates', 'ms' ),
+);
+if ( has_post_thumbnail() ) {
+	$page_header_logo['src'] = get_the_post_thumbnail_url( $post, 'logo_thumbnail' );
+}
+$page_header_args = array(
+	'image' => array(
+		'src' => get_template_directory_uri() . '/assets/images/compact-header-templates.png?ver=' . THEME_VERSION,
+		'alt' => get_the_title(),
+	),
+	'logo'  => $page_header_logo,
+	'title' => get_the_title(),
+	'text'  => do_shortcode( '[urlslab-generator id="6"]' ),
+	'toc'   => true,
+);
+$current_id       = apply_filters( 'wpml_object_id', $post->ID, 'ms_templates' );
+$categories       = get_the_terms( $current_id, 'ms_templates_categories' );
+$categories_url   = get_post_type_archive_link( 'ms_templates' );
+if ( $categories && $categories_url ) {
+	$new_tags = array(
+		'title' => __( 'Categories', 'ms' ),
+	);
+	foreach ( $categories as $category ) {
+		$new_tags['list'][] = array(
+			'href'  => $categories_url . '#' . $category->slug,
+			'title' => $category->name,
+		);
+	}
+	if ( isset( $new_tags['list'] ) ) {
+		$page_header_args['tags'][] = $new_tags;
+	}
+}
+if ( ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_help-desk-software', true ) === 'on' )
+	|| ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_ticketing-software', true ) === 'on' )
+	|| ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_live-chat-software', true ) === 'on' )
+	|| ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_call-center-software', true ) === 'on' )
+	|| ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_social-media', true ) === 'on' )
+	|| ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_customer-portal-software', true ) === 'on' )
+	|| ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_knowledge-base', true ) === 'on' )
+	|| ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_affiliate-program', true ) === 'on' )
+) {
+	$new_tags = array(
+		'title' => __( 'Technologies', 'ms' ),
+	);
+	if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_help-desk-software', true ) === 'on' ) {
+		$new_tags['list'][] = array(
+			'href'  => __( '/help-desk-software/', 'ms' ),
+			'title' => __( 'Help Desk Software', 'ms' ),
+		);
+	}
+	if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_ticketing-software', true ) === 'on' ) {
+		$new_tags['list'][] = array(
+			'href'  => __( '/ticketing-software/', 'ms' ),
+			'title' => __( 'Ticketing Software', 'ms' ),
+		);
+	}
+	if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_live-chat-software', true ) === 'on' ) {
+		$new_tags['list'][] = array(
+			'href'  => __( '/live-chat-software/', 'ms' ),
+			'title' => __( 'Live Chat Software', 'ms' ),
+		);
+	}
+	if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_call-center-software', true ) === 'on' ) {
+		$new_tags['list'][] = array(
+			'href'  => __( '/call-center-software/', 'ms' ),
+			'title' => __( 'Call Center Software', 'ms' ),
+		);
+	}
+	if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_social-media', true ) === 'on' ) {
+		$new_tags['list'][] = array(
+			'href'  => __( '/social-media-customer-service/', 'ms' ),
+			'title' => __( 'Social Media Support', 'ms' ),
+		);
+	}
+	if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_customer-portal-software', true ) === 'on' ) {
+		$new_tags['list'][] = array(
+			'href'  => __( '/customer-portal-software/', 'ms' ),
+			'title' => __( 'Customer Portal Software', 'ms' ),
+		);
+	}
+	if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_knowledge-base', true ) === 'on' ) {
+		$new_tags['list'][] = array(
+			'href'  => __( '/knowledge-base-software/', 'ms' ),
+			'title' => __( 'Knowledge Base Software', 'ms' ),
+		);
+	}
+	if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_affiliate-program', true ) === 'on' ) {
+		$new_tags['list'][] = array(
+			'href'   => __( 'https://www.postaffiliatepro.com/?utm_medium=referral&utm_source=liveagent&utm_campaign=directory', 'ms' ),
+			'title'  => __( 'Affiliate Software', 'ms' ),
+			'target' => '_blank',
+			'rel'    => 'nofollow',
+		);
+	}
+	if ( isset( $new_tags['list'] ) ) {
+		$page_header_args['tags'][] = $new_tags;
+	}
+}
 ?>
-<div class="Post" itemscope itemtype="http://schema.org/Guide">
+<div class="Post Post--sidebar-right" itemscope itemtype="http://schema.org/Guide">
 	<meta itemprop="url" content="<?= esc_url( get_permalink() ); ?>">
-	<span itemprop="publisher" itemscope itemtype="http://schema.org/Organization"><meta itemprop="name" content="PostAffiliatePro"></span>
+	<span itemprop="publisher" itemscope itemtype="http://schema.org/Organization"><meta itemprop="name" content="LiveAgent"></span>
+	<?php get_template_part( 'lib/custom-blocks/compact-header', null, $page_header_args ); ?>
 
-	<div class="Post__header Post__header__narrow academy <?= esc_attr( $header_category ); ?>">
-		<div class="wrapper__wide">
-			<div class="Post__header Post__header__narrow__image">
-				<?php
-				if ( has_post_thumbnail() ) {
-					the_post_thumbnail();
-				}
-				if ( ! has_post_thumbnail() ) {
-					?>
-					<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/category_item_image.jpg" alt="<?php the_title(); ?>" />
-				<?php } ?>
-			</div>
-			<div class="Post__header__narrow__text">
-				<div class="Post__content__breadcrumbs Post__header__narrow__breadcrumbs">
-					<ul>
-						<li><a href="<?php _e( '/templates/', 'ms' ); ?>"><?php _e( 'Templates', 'ms' ); ?></a></li>
-						<li><?= esc_html( $posttitle_filtered ); ?></li>
-					</ul>
-				</div>
-				<h1 class="Post__header__narrow__title">
-					<?php
-					$pagetitle = explode( '^', get_the_title() );
-					if ( isset( $pagetitle[1] ) ) {
-						?>
-						<?php echo esc_html( $pagetitle[0] ) . ' '; ?>
-						<span class="highlight"><?php echo esc_html( $pagetitle[1] ); ?></span>
-						<?php echo esc_html( ' ' . $pagetitle[2] ); ?>
-						<?php
-					} else {
-						echo esc_html( $posttitle_filtered );
-					}
-					?>
-				</h1>
-			</div>
-		</div>
-	</div>
-
-
-	<div class="wrapper__wide Post__container">
+	<div class="wrapper Post__container">
 		<div class="Post__sidebar">
-			<div class="Post__sidebar__categories">
-				<h4 class="Post__sidebar__title"><?php _e( 'Categories', 'ms' ); ?></h4>
-				<ul class="CategoryTags">
-					<?php
-					$current_id = apply_filters( 'wpml_object_id', $post->ID, 'ms_templates' );
-					$categories = get_the_terms( $current_id, 'ms_templates_categories' );
-
-					if ( $categories ) {
-						foreach ( $categories as $category ) {
-							$category_id    = $category->term_id;
-							$category_name  = $category->name;
-							$category_color = get_term_meta( $category_id, 'category_color', true );
-							?>
-							<li class="CategoryTag <?= esc_attr( $category_color ); ?>">
-								<a href="../#<?= esc_attr( $category->slug ); ?>" title="<?= esc_attr( $category_name ); ?>"><?= esc_html( $category_name ); ?></a>
-							</li>
-							<?php
-						}
-					}
-					?>
-				</ul>
+			<div class="Signup__sidebar-wrapper">
+				<?= do_shortcode( '[signup-sidebar js-sticky="true"]' ); ?>
 			</div>
-
-			<?php if ( sidebar_toc() !== false ) { ?>
-				<div class="SidebarTOC-wrapper">
-					<div class="SidebarTOC Post__SidebarTOC">
-						<strong class="SidebarTOC__title"><?php _e( 'Contents', 'ms' ); ?></strong>
-						<div class="SidebarTOC__slider slider splide">
-							<div class="splide__track">
-								<ul class="SidebarTOC__content splide__list">
-									<?= wp_kses_post( sidebar_toc() ); ?>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			<?php } ?>
 		</div>
-
-		<div class="Signup__sidebar-wrapper">
-			<?= do_shortcode( '[signup-sidebar]' ); ?>
-		</div>
-
 		<div class="Post__content">
+
 			<div class="Content" itemprop="text">
 				<?php the_content(); ?>
-				<?php echo do_shortcode( '[urlslab-faq]' ); ?>
 
 				<div class="Post__buttons">
 					<a href="<?php _e( '/templates/', 'ms' ); ?>" class="Button Button--outline Button--back"  onclick="_paq.push(['trackEvent', 'Activity', 'Templates', 'Back to Templates'])"><span><?php _e( 'Back to Templates', 'ms' ); ?></span></a>
