36cf9d51455a580368ed14947040d6d14f2c6e1d
thujcak <thujcak@qualityunit.com>
2023-10-11 20:29:18 +0200

    feat: changed directory archive with dynamical filter

--- templates/content-archive-ms_directory.php
+++ templates/content-archive-ms_directory.php
@@ -14,6 +14,28 @@ $page_header_args = array(
 	'title' => $page_header_title,
 	'text'  => $page_header_text,
 );
+
+$glossaryposts = get_posts(
+	array(
+		'post_type'   => 'ms_directory',
+		'post_status' => 'publish',
+		'numberposts' => -1,
+		'order'       => 'ASC',
+		'orderby'     => 'title',
+	)
+);
+
+$index = array();
+foreach ( $glossaryposts as $glossarypost ) {
+	$posttitle = $glossarypost->post_title;
+	$first_character = substr( $posttitle, 0, 1 );
+
+	if ( ( ctype_upper( $first_character ) || ctype_digit( $first_character ) ) && ! in_array( $first_character, $index ) ) {
+		$index[] = $first_character;
+	}
+}
+
+
 ?>
 <div id="archive" class="Archive" itemscope itemtype="https://schema.org/DefinedTermSet">
 	<?php get_template_part( 'lib/custom-blocks/compact-header', null, $page_header_args ); ?>
@@ -21,9 +43,8 @@ $page_header_args = array(
 		<div class="wrapper">
 			<div class="Archive__filter__item">
 				<ul>
-					<?php $categories = get_categories( array( 'taxonomy' => 'ms_directory_categories' ) ); ?>
-					<?php foreach ( $categories as $category ) { ?>
-						<li><a href="#<?= esc_attr( $category->slug ); ?>" title="<?= esc_attr( $category->name ); ?>"><?= esc_html( $category->name ); ?></a></li>
+					<?php foreach ( $index as $index_item ) { ?>
+						<li class="Index__top--item"><a href="#item-<?= esc_attr( $index_item ); ?>" title="<?= esc_attr( $index_item ); ?>"><?= esc_html( $index_item ); ?></a></li>
 					<?php } ?>
 				</ul>
 			</div>
@@ -32,39 +53,32 @@ $page_header_args = array(
 
 	<div class="wrapper Archive__container Archive__container--glossary">
 		<div class="Archive__container__content list">
-			<?php $categories = get_categories( array( 'taxonomy' => 'ms_directory_categories' ) ); ?>
-			<?php foreach ( $categories as $category ) { ?>
-				<div id="<?= esc_attr( $category->slug ); ?>" class="Archive__container__content__item">
-					<h2 class="Archive__container__content__item__title"><?= esc_html( $category->name ); ?></h2>
+			<?php
+			foreach ( $index as $index_item ) {
+				?>
+				<div id="item-<?= esc_attr( $index_item ); ?>" class="Archive__container__content__item">
+					<h2 class="Archive__container__content__item__title"><?= esc_html( $index_item ); ?></h2>
 					<div class="Archive__container__content__item__content">
 						<ul>
 							<?php
-							$query_glossary_posts = new WP_Query(
-								array(
-									'ms_directory_categories' => $category->slug,
-									// @codingStandardsIgnoreLine
-									'posts_per_page' => 500,
-									'orderby'        => 'title',
-									'order'          => 'ASC',
-								)
-							);
-							while ( $query_glossary_posts->have_posts() ) :
-								$query_glossary_posts->the_post();
-								?>
-								<?php
-								if ( get_post_meta( get_the_ID(), 'mb_directory_mb_directory_company-name', true ) ) {
-									$post_title = get_post_meta( get_the_ID(), 'mb_directory_mb_directory_company-name', true );
-								} else {
-									$post_title = get_the_title();
+
+							foreach ( $glossaryposts as $glossarypost ) {
+								$postid          = $glossarypost->ID;
+								$posttitle       = $glossarypost->post_title;
+								$first_character = substr( $posttitle, 0, 1 );
+
+								if ( $first_character === $index_item ) {
+									?>
+									<li itemscope="" itemtype="https://schema.org/DefinedTerm"><a href="<?= esc_url( get_permalink( $postid ) ); ?>" itemprop="url"><span itemprop="name" data-search><?= esc_html( $posttitle ); ?></span></a></li>
+									<?php
 								}
-								?>
-								<li itemscope itemtype="https://schema.org/DefinedTerm"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( $post_title ); ?> <?php _e( 'customer support contacts', 'ms' ); ?>" itemprop="url"><span itemprop="name"><?php echo esc_html( $post_title ); ?></span></a></li>
-							<?php endwhile; ?>
-							<?php wp_reset_postdata(); ?>
+							}
+							?>
 						</ul>
 					</div>
 				</div>
 			<?php } ?>
+
 		</div>
 	</div>
 </div>
