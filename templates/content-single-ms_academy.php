1e94226bb61b0244efa194687bafa07a084c03b1
thujcak <thujcak@qualityunit.com>
2023-10-31 13:46:54 +0100

    fix: replaced old code with new shortcode

--- templates/content-single-ms_academy.php
+++ templates/content-single-ms_academy.php
@@ -22,7 +22,7 @@ $page_header_args = array(
 	),
 	'logo'  => $page_header_logo,
 	'title' => get_the_title(),
-	'text'  => get_the_excerpt( $post ),
+	'text'  => do_shortcode( '[urlslab-generator id="4"]' ),
 	'toc'   => true,
 );
 $current_id       = apply_filters( 'wpml_object_id', $post->ID, 'ms_academy' );
