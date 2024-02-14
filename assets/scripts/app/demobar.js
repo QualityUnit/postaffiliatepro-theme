6440d4905991a947013d21888b1e79e027bb17c8
thujcak <thujcak@qualityunit.com>
2024-02-14 13:43:57 +0100

    fix: added js condition for demo bar and disable demo bar on  the trial page de version

--- assets/scripts/app/demobar.js
+++ assets/scripts/app/demobar.js
@@ -3,37 +3,40 @@
 const demobar = document.querySelector( '#demobar' );
 if ( demobar ) {
 	const demobarButton = document.querySelector( '.DemoBar__button' );
-	const demobarUrl = demobarButton.getAttribute( 'href' );
 
-	document
-		.querySelector( '.DemoBar__close' )
-		.addEventListener( 'click', () => {
-			demobar.classList.add( 'hide' );
-			demobar.classList.remove( 'show' );
-			setTimeout( () => {
-				demobar.classList.remove( 'visible' );
-			}, 500 );
+	if ( demobarButton ) {
+		const demobarUrl = demobarButton.getAttribute( 'href' );
+
+		document
+			.querySelector( '.DemoBar__close' )
+			.addEventListener( 'click', () => {
+				demobar.classList.add( 'hide' );
+				demobar.classList.remove( 'show' );
+				setTimeout( () => {
+					demobar.classList.remove( 'visible' );
+				}, 500 );
+				setCookie( 'demobar', 'yes', 14 );
+			} );
+
+		demobarButton.addEventListener( 'click', ( event ) => {
+			event.preventDefault();
 			setCookie( 'demobar', 'yes', 14 );
-		} );
 
-	demobarButton.addEventListener( 'click', ( event ) => {
-		event.preventDefault();
-		setCookie( 'demobar', 'yes', 14 );
-
-		setTimeout( () => {
-			window.location.pathname = demobarUrl;
-		}, 10 );
-	} );
-
-	if ( ! getCookie( 'demobar' ) && getCookie( 'cookieLaw' ) ) {
-		window.addEventListener( 'load', () => {// eslint-disable-line
 			setTimeout( () => {
-				demobar.classList.add( 'visible' );
-			}, 500 );
+				window.location.pathname = demobarUrl;
+			}, 10 );
 		} );
 
-		setTimeout( () => {
-			demobar.classList.add( 'show' );
-		}, 5000 );
+		if ( ! getCookie( 'demobar' ) && getCookie( 'cookieLaw' ) ) {
+			window.addEventListener( 'load', () => {// eslint-disable-line
+				setTimeout( () => {
+					demobar.classList.add( 'visible' );
+				}, 500 );
+			} );
+
+			setTimeout( () => {
+				demobar.classList.add( 'show' );
+			}, 5000 );
+		}
 	}
 }
