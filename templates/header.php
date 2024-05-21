a9e0277323469c7d76cfd573a0d2535818cc9f99
thujcak <thujcak@qualityunit.com>
2024-05-21 12:38:56 +0200

    fix: fixed book and call button on the header

--- templates/header.php
+++ templates/header.php
@@ -85,7 +85,7 @@
 			</div>
 
 			<div class="Header__navigation__buttons">
-				<a href="<?= esc_url( $post_slug ); ?><?php _e( '/call/', 'ms' ); ?>"
+				<a href="<?= $post_slug == '/network' ? _e( '/call/', 'ms' ) : esc_url( $post_slug ) . _e( '/call/', 'ms' ); ?>"
 					 class="Button Button--outline">
 					<span><?php _e( 'Book a Call', 'ms' ); ?></span>
 				</a>
