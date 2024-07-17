7b5375990e4476d223c80a9ded1c5565d1335306
Jozef Remen <jozef.remen@gmail.com>
2024-07-17 14:22:43 +0200

    fix: tracking scripts (#811)

--- base-scripts.php
+++ base-scripts.php
@@ -15,6 +15,8 @@
 		return null
 	}
 
+	const mobile = window.matchMedia('(max-width: 768px)');
+
 	const acceptButton = document.querySelector( ".Kolaciky__button--yes" )
 	const trialButton = document.querySelector( "button[data-id=createButtonmain]" )
 
@@ -29,8 +31,11 @@
 			}, 5000 )
 		}
 
+		consentGranted();
+		grafana();
+		postAffiliate();
 		if ( typeof createButton == 'function' ) {
-			createButton()
+			createButton();
 		}
 	} )
 
@@ -38,18 +43,145 @@
 		trialButton.addEventListener( "click", () => {
 			setCookie( 'cookieLaw', 'yes', 14 )
 			document.querySelector( '.Kolaciky' ).classList.add( 'hide' )
+
+			consentGranted();
+			grafana();
+			postAffiliate();
 		});
 	}
 </script>
 
 <!-- Google Tag Manager - No Cookies -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DLV457J560"></script>
+<script>
+	function loadGoogle() {
+		const body  = document.body;
+		const gtag1 = document.createElement('script');
+		gtag1.async = true;
+		gtag1.src = "https://www.googletagmanager.com/gtag/js?id=G-DLV457J560";
+
+		body.insertAdjacentElement('beforeend', gtag1);
+	}
+
+	if ( ! mobile.matches ) {
+		loadGoogle()
+	}
+
+	if ( mobile.matches && getCookieFrontend( "cookieLaw" ) ) {
+		loadGoogle()
+	}
+</script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-DLV457J560');
+	gtag('consent', 'default', {
+		'ad_user_data': 'granted',
+		'analytics_storage': 'granted'
+	})
+
+	gtag('consent', 'default', {
+		'ad_user_data': 'denied',
+		'analytics_storage': 'denied',
+		'region': ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'IS', 'LI', 'NO']
+	})
+
+  gtag('config', 'G-DLV457J560', {
+		'allow_enhanced_conversions': true,
+		'linker': {
+			'domains': [
+				'postaffiliatepro.com',
+				'postaffiliatepro.fr',
+				'postaffiliatepro.de',
+				'postaffiliatepro.hu',
+				'postaffiliatepro.com.br',
+				'postaffiliatepro.es',
+				'postaffiliatepro.nl',
+				'postaffiliatepro.it',
+				'postaffiliatepro.pl',
+				'postaffiliatepro.sk',
+				'support.liveagent.com',
+				'ladesk.com'
+			]
+		}
+	});
+
+	function consentGranted() {
+		gtag('consent', 'update', {
+			'ad_user_data': 'granted',
+			'analytics_storage': 'granted'
+		})
+	}
+</script>
+
+
+<!-- Post Affiliate Pro -->
+<script type="text/javascript">
+	function postAffiliate() {
+		(function(d,t) {
+			var script = d.createElement(t); script.id= 'pap_x2s6df8d'; script.async = true;
+			script.src = '//pap.qualityunit.com/scripts/m3j58hy8fd';
+			script.onload = script.onreadystatechange = function() {
+				var rs = this.readyState; if (rs && (rs != 'complete') && (rs != 'loaded')) return;
+				PostAffTracker.setAccountId('default1');
+				if ( !getCookieFrontend( "cookieLaw" ) ) {
+					try {
+						PostAffTracker.disableTrackingMethod('C');
+						PostAffTracker.track();
+					} catch (e) {}
+				}
+				if ( getCookieFrontend( "cookieLaw" ) ) {
+					try {
+						PostAffTracker.enableTrackingMethods();
+						PostAffTracker.track();
+					} catch (e) {}
+				}
+			}
+			var placeholder = document.getElementById('papPlaceholder');
+			placeholder.parentNode.insertBefore(script, placeholder);
+		})(document, 'script');
+	}
+
+	if ( ! mobile.matches ) {
+		postAffiliate()
+	}
+
+	if ( mobile.matches && getCookieFrontend( "cookieLaw" ) ) {
+		postAffiliate()
+	}
+</script>
+
+<!-- Grafana -->
+<script>
+	function grafana(cookie = true) {
+		var _paq = window._paq || [];
+		window._paq = _paq;
+
+		if (cookie === false) {
+			_paq.push(["disableCookies"]);
+		}
+
+		// _paq.push(['enableLinkTracking']);
+		_paq.push(['trackPageView']);
+		_paq.push(['enableCrossDomainLinking']);
+		(function() {
+			_paq.push(['setSiteId', 'PAP-web']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.defer=true; g.src='//analytics.qualityunit.com/i.js'; s.parentNode.insertBefore(g,s);
+		})();
+	}
+
+	if ( ! mobile.matches ) {
+		if ( getCookieFrontend( "cookieLaw" ) ) {
+			grafana()
+		} else {
+			grafana(false)
+		}
+	}
+
+	if ( mobile.matches && getCookieFrontend( "cookieLaw" ) ) {
+		grafana()
+	}
 </script>
 
 <!-- LiveAgent - Chat Button -->
