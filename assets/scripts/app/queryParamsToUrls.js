b2d7753c1241caf3a48335eb1b27f086b2e3ba2f
Tomáš Hujčák <134269593+thujcak@users.noreply.github.com>
2024-08-26 13:45:34 +0200

    [3162] Disabled js for query param (#837)
    * feat: disabled js for query param
    
    * feat: disabled js for query param and fix function related posts

--- assets/scripts/app/queryParamsToUrls.js
+++ assets/scripts/app/queryParamsToUrls.js
@@ -1,42 +1,42 @@
-// adds all query params from incoming URL to urls on page
-
-( () => {
-	const currentHostname = window.location.hostname;
-
-	// If the hostname contains 'admin', exit the script
-	if ( currentHostname.includes( 'admin' ) ) {
-		return;
-	}
-
-	const queryParams = new URL( window.location ).searchParams;
-	const blacklist = [
-		'p',
-		'attachment_id',
-		'_ga',
-		'text',
-	];
-	const allUrls = document.querySelectorAll( 'a[href]' );
-
-	allUrls.forEach( ( url ) => {
-		const currentHref = url.getAttribute( 'href' );
-
-		// Skip links that are empty or placeholders
-		if ( currentHref === '#' || currentHref === '#0' || currentHref === '' ) {
-			return;
-		}
-
-		const currentUrl = new URL( url.href );
-
-		// Add query parameters to the URL if not blacklisted
-		queryParams.forEach( ( value, key ) => {
-			if ( ! blacklist.includes( key ) ) {
-				currentUrl.searchParams.set( key, value );
-			}
-		} );
-
-		// Update the URL if the hostname contains 'postaffiliate'
-		if ( currentUrl.hostname.includes( 'postaffiliate' ) ) {
-			url.href = currentUrl.toString();
-		}
-	} );
-} )();
+// // adds all query params from incoming URL to urls on page
+//
+// ( () => {
+// 	const currentHostname = window.location.hostname;
+//
+// 	// If the hostname contains 'admin', exit the script
+// 	if ( currentHostname.includes( 'admin' ) ) {
+// 		return;
+// 	}
+//
+// 	const queryParams = new URL( window.location ).searchParams;
+// 	const blacklist = [
+// 		'p',
+// 		'attachment_id',
+// 		'_ga',
+// 		'text',
+// 	];
+// 	const allUrls = document.querySelectorAll( 'a[href]' );
+//
+// 	allUrls.forEach( ( url ) => {
+// 		const currentHref = url.getAttribute( 'href' );
+//
+// 		// Skip links that are empty or placeholders
+// 		if ( currentHref === '#' || currentHref === '#0' || currentHref === '' ) {
+// 			return;
+// 		}
+//
+// 		const currentUrl = new URL( url.href );
+//
+// 		// Add query parameters to the URL if not blacklisted
+// 		queryParams.forEach( ( value, key ) => {
+// 			if ( ! blacklist.includes( key ) ) {
+// 				currentUrl.searchParams.set( key, value );
+// 			}
+// 		} );
+//
+// 		// Update the URL if the hostname contains 'postaffiliate'
+// 		if ( currentUrl.hostname.includes( 'postaffiliate' ) ) {
+// 			url.href = currentUrl.toString();
+// 		}
+// 	} );
+// } )();
