75396ebbfa23b8a8f0272b944977507e352f93c0
Tomáš Hujčák <134269593+thujcak@users.noreply.github.com>
2024-11-26 09:52:11 +0100

    fix: countdown in the safari and copy coupon button (#906)

--- assets/scripts/app/app.js
+++ assets/scripts/app/app.js
@@ -485,20 +485,26 @@
 	}
 
 	/* Copy to clipboard */
-	if ( queryAll( '.Copy ' ).length > 0 ) {
-		queryAll( '.Copy ' ).forEach( ( item ) => {
+	if ( document.querySelectorAll( '.Copy ' ).length > 0 ) {
+		document.querySelectorAll( '.Copy ' ).forEach( ( item ) => {
 			item.querySelector( '.Button--copy' ).addEventListener(
 				'click',
 				() => {
 					const thisCopy = item;
-					const copyText = thisCopy.querySelector( 'textarea' );
+					const copyText = thisCopy.querySelector(
+						'.textarea-pseudo'
+					).innerText;
 					const defaultText = thisCopy.querySelector(
 						'.Button--copy span'
 					).textContent;
 
-					copyText.select();
+					const textArea = document.createElement( 'textarea' );
+					textArea.value = copyText;
+					document.body.appendChild( textArea );
+					textArea.select();
 
 					document.execCommand( 'copy' );
+					document.body.removeChild( textArea );
 
 					thisCopy.querySelector( '.Button--copy span' ).textContent =
 						'Copied!';
